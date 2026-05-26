5a942f3dffd70870e57b38670374826ce5990edc
Curtis Bezencon <cbezenco@c1r9s2.42lausanne.ch>
2026-05-26 13:22:10 +0200

    lets go index

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   Server.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: strieste <strieste@student.42.ch>          +#+  +:+       +#+        */
+/*   By: cbezenco <cbezenco@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 14:38:06 by strieste          #+#    #+#             */
-/*   Updated: 2026/05/21 14:53:12 by strieste         ###   ########.fr       */
+/*   Updated: 2026/05/26 13:21:31 by cbezenco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/Server.hpp"
 #include <iostream>
+#include <fstream>
 
 /*	Linux version	*/
 Server::Server()
@@ -74,9 +75,22 @@ void Server::StartServer()
 					std::cout << buff << std::endl;
 					std::cout << "###	End client message	###\n" << std::endl;
 					struct stat sendClient;
-					stat("../index.html", &sendClient);
+					stat("index.html", &sendClient);
 					std::cout << sendClient.st_size << std::endl;
-					write(changeList[i].data.fd, "HTTP/1.1 200 OK\r\nContent-Length:13\r\n\r\nHello, world!", 52);
+					std::string response = "HTTP/1.1 200 OK\r\nContent-Type:text/html\r\nContent-Length:405";
+					response.append("\r\n\r\n");
+					std::ifstream ifs;
+					ifs.open("index.html");
+					if (ifs.is_open()) {
+						std::cout << "All good" << std::endl;
+					}
+					std::string line;
+					while (std::getline(ifs, line)) {
+						response.append(line);
+						response.append("\n");					
+					}
+					write(changeList[i].data.fd, response.c_str(), response.size());
+					//write(changeList[i].data.fd, "HTTP/1.1 200 OK\r\nContent-Length:13\r\n\r\nHello, world!", 52);
 				}
 			}
 			NbRequest++;
