b2ac9bb0bd0b03da835948781f1cc6c9be689496
Seully Trieste <seully@MacS.local>
2026-05-26 08:21:16 +0200

    Add ConfigLocation and ConfigServer class

--- header/Server.hpp
+++ header/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: strieste <strieste@student.42.ch>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 14:34:40 by strieste          #+#    #+#             */
-/*   Updated: 2026/05/21 14:51:51 by strieste         ###   ########.fr       */
+/*   Updated: 2026/05/25 15:06:03 by strieste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@
 # include <sys/socket.h>
 # include <netinet/in.h>
 # include <sys/stat.h>
-// # include <sys/event.h>	// MacOS
-# include <sys/epoll.h>	// Linux
+# include <sys/event.h>	// MacOS
+// # include <sys/epoll.h>	// Linux
 # include <iostream>
 # include <unistd.h>
 # include <errno.h>
+# include <vector>
+# include "ConfigServer.hpp"
 
 # define MAX_EVENTS 10
 
@@ -32,16 +34,17 @@ class Server
 		Server(Server const &copy);
 		~Server();
 		Server&	operator=(Server const &copy);
-		
+
 		void	StartServer();
 		void	StopServer();
 
 	private:
+		int _fdServer;
+		int	_NumberConfig;
+		std::vector<ConfigServer>	_configServer;
+
 		int	_socket;
 		int	_port;
-		// int _kq;
-		int _fdServer;
-		// std::string	_ipAddress;
 		struct sockaddr_in	_sockAddress;
 
 		// struct epoll_event	_event;
